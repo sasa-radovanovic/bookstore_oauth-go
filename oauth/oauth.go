0e97398c35b63805037d782f0ebf0f0f27aaa9d4
Sasa Radovanovic <sasa.radovanovic@booking.com>
2020-07-30 09:07:37 +0200

    adding centralised oauth module

---
+++ oauth/oauth.go
@@ -0,0 +1,120 @@
+package oauth
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/golang-restclient/rest"
+	"github.com/sasa-radovanovic/bookstore_oauth-go/errors"
+)
+
+const (
+	headerXPublic    = "X-Public"
+	headerXClientID  = "X-Client-Id"
+	headerXCallerID  = "X-Caller-Id"
+	paramAccessToken = "access_token"
+)
+
+var (
+	oauthRestClient = rest.RequestBuilder{
+		BaseURL: "http://localhost:8082",
+		Timeout: 200 * time.Millisecond,
+	}
+)
+
+type accessToken struct {
+	ID       string `json:"id"`
+	UserID   int64  `json:"user_id"`
+	ClientID int64  `json:"client_id"`
+}
+
+// IsPublic is checking if request is public
+func IsPublic(request *http.Request) bool {
+	if request == nil {
+		return true
+	}
+	return request.Header.Get(headerXPublic) == "true"
+}
+
+// GetCallerID Retrieve Caller ID
+func GetCallerID(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	callerID, err := strconv.ParseInt(request.Header.Get(headerXCallerID), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return callerID
+}
+
+// GetClientID Retrieve Client ID
+func GetClientID(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	clientID, err := strconv.ParseInt(request.Header.Get(headerXClientID), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return clientID
+}
+
+// AuthenticateRequest is used for request authentication
+func AuthenticateRequest(request *http.Request) *errors.RestErr {
+	if request == nil {
+		return nil
+	}
+
+	cleanRequest(request)
+
+	accessTokenID := strings.TrimSpace(request.URL.Query().Get(paramAccessToken))
+	if accessTokenID == "" {
+		return nil
+	}
+
+	at, err := getAccessToken(accessTokenID)
+	if err != nil {
+		if err.Code == http.StatusNotFound {
+			return nil
+		}
+		return err
+	}
+
+	request.Header.Add(headerXCallerID, fmt.Sprintf("%v", at.UserID))
+	request.Header.Add(headerXClientID, fmt.Sprintf("%v", at.ClientID))
+	return nil
+}
+
+func cleanRequest(request *http.Request) {
+	if request == nil {
+		return
+	}
+	request.Header.Del(headerXClientID)
+	request.Header.Del(headerXCallerID)
+}
+
+func getAccessToken(accessTokenID string) (*accessToken, *errors.RestErr) {
+	response := oauthRestClient.Get(fmt.Sprintf("/oauth/access_token/%s", accessTokenID))
+	fmt.Println("Response is ", response)
+	if response == nil || response.Response == nil {
+		return nil, errors.NewInternalServerError("invalid rest client response when trying to get access token")
+	}
+	if response.StatusCode > 299 {
+		var restErr errors.RestErr
+		err := json.Unmarshal(response.Bytes(), &restErr)
+		if err != nil {
+			return nil, errors.NewInternalServerError("invalid error interface when trying to login user")
+		}
+		return nil, &restErr
+	}
+	var at accessToken
+	if err := json.Unmarshal(response.Bytes(), &at); err != nil {
+		return nil, errors.NewInternalServerError("error trying to unmarshal access token response")
+	}
+	return &at, nil
+}
